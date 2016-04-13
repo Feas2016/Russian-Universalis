34e04e044fd4fd4717e3ee596d2ef5998030b955
Feas2016 <else2016@yandex.ru>
2016-04-13 20:09:46 +0300

    Первый блин комом 1.16
    - Русик используем МП и удаляем из неё папку interface
    http://steamcommunity.com/sharedfiles/filedetails/?id=537911801
    - Вылет происходит при попытке запустить партию

---
+++ STT_E/common/defines/00_typus_defines.lua
@@ -0,0 +1,41 @@
+-- Custom defines for Typus to fix the main menu camera:
+NDefines.NGame.MAX_RANDOM_NEW_WORLD = "2.2.2"
+NDefines.NGame.END_DATE = "1850.1.1"
+
+NDefines.NFrontend.CAMERA_LOOKAT_X = 2787.0
+NDefines.NFrontend.CAMERA_LOOKAT_Z = 1845.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 2787.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 1845.0
+NDefines.NFrontend.CAMERA_START_X = 2787.0
+NDefines.NFrontend.CAMERA_START_Z = 1755.0
+NDefines.NFrontend.CAMERA_END_X = 2787.0
+NDefines.NFrontend.CAMERA_END_Z = 1700.0
+NDefines.NFrontend.FRONTEND_POS_X = 2787.0
+NDefines.NFrontend.FRONTEND_POS_Z = 1825.0
+NDefines.NFrontend.FRONTEND_LOOK_X = 2787.0
+NDefines.NFrontend.FRONTEND_LOOK_Z = 1845.0
+NDefines.NFrontend.SETTINGS_POS_X = 2787.0
+NDefines.NFrontend.SETTINGS_POS_Z = 1228.0
+NDefines.NFrontend.SETTINGS_LOOK_X = 2787.0
+NDefines.NFrontend.SETTINGS_LOOK_Z = 1499.0
+NDefines.NFrontend.MP_OPTIONS_POS_X = 2787.0
+NDefines.NFrontend.MP_OPTIONS_POS_Z = 1398.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_X = 2787.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 1459.0
+NDefines.NFrontend.TUTORIAL_POS_X = 2724.0
+NDefines.NFrontend.TUTORIAL_POS_Z = 1654.0
+NDefines.NFrontend.TUTORIAL_LOOK_X = 2710.0
+NDefines.NFrontend.TUTORIAL_LOOK_Z = 1792.0
+NDefines.NFrontend.CONTENT_POS_X = 2787.0
+NDefines.NFrontend.CONTENT_POS_Z = 1642.0
+NDefines.NFrontend.CONTENT_LOOK_X = 2787.0
+NDefines.NFrontend.CONTENT_LOOK_Z = 1845.0
+NDefines.NFrontend.CREDITS_POS_X = 2911.0
+NDefines.NFrontend.CREDITS_POS_Z = 1824.0
+NDefines.NFrontend.CREDITS_LOOK_X = 2916.0
+NDefines.NFrontend.CREDITS_LOOK_Z = 2027.0
+
+NDefines.NCountry.ALLOW_ZERO_BASE_VALUES = 1
+NDefines.NCountry.ALLOW_ESTATE_AND_PARLIAMENT_SEAT_IN_PROVINCE = 1
+NDefines.NCountry.ALLOW_ESTATE_IN_CAPITAL = 1
+
